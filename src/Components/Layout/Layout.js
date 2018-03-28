a55bb2c9b95e5a8ef3a29063ccaafdb4a2b208b3
Anton Tengroth <antte227@student.liu.se>
2018-03-28 19:39:58 +0200

    Added a signup page

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
 import Toolbar from './Toolbar';
 import Login from '../LoginPage/Login'
 import classes from './Layout.css'
--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import FeedPage from "./Components/FeedPage/FeedPage";
 import NotFound from "./Components/Layout/NotFound";
 import Login from './Components/LoginPage/Login';
+import SignUp from "./Components/SignUpPage/SignUp";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={FeedPage} />
     <Route path="/login" exact component={Login} />
+    <Route path="/signup" exact component={SignUp} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
