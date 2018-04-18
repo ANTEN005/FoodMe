c48383ba7aa01671785d6c20cad19e28f923509d
Anton Tengroth <antte227@student.liu.se>
2018-04-18 16:44:10 +0200

    initial api commit, with a dummy auth route

---
+++ foodme-api/src/index.js
@@ -0,0 +1,14 @@
+    var express = require("express");
+    var path = require("path");
+
+    const app = express();
+
+    app.post('/api/auth', (req, res) => {
+        res.status(400).json({errors: {global: "Invalid credentials"}});
+    })
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
+
+    app.listen(8080, () => console.log('Running on localhost'))
