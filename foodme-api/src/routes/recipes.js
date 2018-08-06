4796e2681daac561205cb601421b1652be1b5fd2
Anton Tengroth <antte227@student.liu.se>
2018-08-06 11:27:15 +0200

    Get recipes from database instead of dummy api

--- foodme-api/src/routes/recipes.js
+++ foodme-api/src/routes/recipes.js
@@ -1,24 +1,22 @@
 import express from 'express';
+import Recipe from '../models/Recipe';
 
 const router = express.Router();
 
 router.get('/search', (req, res) => {
-    res.json({
-        recipes: [
-            {
-                id: 1,
-                name: "Pasta",
-                authors: "Italian"
-            },
-            {
-                id: 2,
-                name: "Hamburger",
-                authors: "Fast food"
-            }
-
-        ]
-    })
+    Recipe.find({}).exec((err,recipes) => {
+        if(err){
+        return res.json({'success':false,'message':'Some Error'});
+        }
+        if(recipes.length){
+          return res.json({'success':true,'message':'Successfully fetched all your recipes',recipes});
+        }
+        else{
+          return res.json({'success':false,'message':'No recipes were found'});
+        }
+      });
 
 });
+
 
 export default router;
