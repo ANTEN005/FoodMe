3abe0828612a043cc516f1ff4d298e20788a0ed6
Anton Tengroth <antte227@student.liu.se>
2018-08-10 11:20:51 +0200

    Added RecipeDetailsPage

--- src/Components/Pages/RecipeDetailsPage.js
+++ src/Components/Pages/RecipeDetailsPage.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Container, Header, Segment, Grid, Divider, Image} from 'semantic-ui-react';
 
 class RecipeDetailsPage extends React.Component {
     render() {
-      return (
-        <div>
-            <h1>Recipe details</h1>
-        </div>
-      )}
+      const { selectedRecipe} = this.props;
+      return(
+      <Grid columns={3} relaxed>
+      <Grid.Column>
+        <Header as='h2' textAlign='center'>{selectedRecipe.name} </Header>
+        <Segment basic>
+          <Image src={'/assets/food2.jpg'} size='medium' floated='left' />
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Header as='h5' textAlign='center'>Ingredients</Header>
+        <Segment basic>{selectedRecipe.ingredients}</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Header as='h5' textAlign='center'>Description</Header>
+        <Segment basic>{selectedRecipe.description}</Segment>
+      </Grid.Column>
+      </Grid>
+    )}
     }
-  export default RecipeDetailsPage;
+
+    const mapStateToProps = (state) => ({
+      selectedRecipe: state.selectedRecipe,
+    })
+
+ export default connect(mapStateToProps)(RecipeDetailsPage);
