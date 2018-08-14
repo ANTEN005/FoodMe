2d4d8051f281dbc94a31ac80141e8a3eafc152a2
Anton Tengroth <antte227@student.liu.se>
2018-08-14 16:49:52 +0200

    Recipes that you browse are now clickable, and you can see recipeDetails

--- src/Components/Pages/RecipePage.js
+++ src/Components/Pages/RecipePage.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import {Segment} from 'semantic-ui-react';
 import SearchRecipeForm from '../forms/SearchRecipeForm';
+import {getRecipeDetails} from '../../actions/recipes';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 class RecipePage extends React.Component{
     state = {
         recipe: null
     }
 
+    onRecipeSelect = recipe => {
+        this.setState({ recipe });
+        console.log(recipe);
+        this.props.dispatch(getRecipeDetails(recipe));
+        this.props.history.push("/recipedetails");
+
+
+      };
+    
     render(){
         return(
             <Segment>
                 <h1> Add new recipe to your collection</h1>
-                <SearchRecipeForm/>>
+                <SearchRecipeForm onRecipeSelect={this.onRecipeSelect}/>
             </Segment>
         );
     }
 }
+const mapStateToProps = state => ({
+    recipe: state.recipe
+  });
 
-export default RecipePage;
+  RecipePage.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired
+  };
+
+export default connect(mapStateToProps)(RecipePage);;
--- src/Components/forms/SearchRecipeForm.js
+++ src/Components/forms/SearchRecipeForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Form, Dropdown} from 'semantic-ui-react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 //when user types we will send request to server, who will ping api
 //which will send back data to server who will parse this data and feed it to us
-// we will populate options and recipes, so when user chooses its book we do not 
+// we will populate options and recipes, so when user chooses its recipe we do not 
 //need to make another request to the server
 class SearchRecipeForm extends React.Component{
     state = {
@@ -21,6 +22,13 @@ class SearchRecipeForm extends React.Component{
         });
         this.timer=setTimeout(this.fetchOptions, 1000);
     }
+
+    onChange = (e, data) => { 
+        this.setState({ query: data.value });
+        console.log(this.state.recipes[data.value]);
+        this.props.onRecipeSelect(this.state.recipes[data.value]);
+      };
+    
     fetchOptions = () => {
         if(!this.state.query) return;
         this.setState({loading: true});
@@ -31,14 +39,16 @@ class SearchRecipeForm extends React.Component{
             const options = [];
             const recipesHash = {};
             recipes.forEach(recipe => {
-              recipesHash[recipe.id] = recipe;
+              recipesHash[recipe._id] = recipe;
               options.push({
-                key: recipe.id,
-                value: recipe.id,
+                key: recipe._id,
+                value: recipe._id,
                 text: recipe.name
               });
             });
             this.setState({ loading: false, options, recipes: recipesHash });
+            console.log(options);
+            console.log(recipes);
           });
 
     }
@@ -54,10 +64,16 @@ class SearchRecipeForm extends React.Component{
                     onSearchChange={this.onSearchChange} //when user types something
                     options={this.state.options}
                     loading={this.state.loading}
+                    onChange={this.onChange}
                 />
             </Form>
         );
     }
 }
+
+SearchRecipeForm.propTypes = {
+    onRecipeSelect: PropTypes.func.isRequired
+  };
+  
 
 export default SearchRecipeForm;
